7f017c6faa17064d287b81b2af6bf97e308c2d86
Finnvb <finnvb@gmail.com>
2022-12-14 19:42:00 +0100

    Update HamburgermenuWeek2.svelte

--- src/lib/components/HamburgermenuWeek2.svelte
+++ src/lib/components/HamburgermenuWeek2.svelte
@@ -75,7 +75,7 @@
 		align-items: center;
 		position: relative;
 		background-color: var(--red);
-		background-image: url('/bg1.png');
+		background-image: url('./bg1.png');
 		opacity: 10%;
 		height: 100vh;
 		background-repeat: repeat;
@@ -124,7 +124,7 @@
 		top: 100%;
 		width: 20vw;
 		height: 100vh;
-		background-image: url('/img1.jpg');
+		background-image: url('./img1.jpg');
 		background-repeat: repeat;
 		background-position: bottom;
 		background-size: cover;
@@ -138,36 +138,36 @@
 	} */
 
 	.img2{
-		background-image: url('/img2.jpg');
+		background-image: url('./img2.jpg');
 
 	}
 
 	.img3{
-		background-image: url('/img3.jpg');
+		background-image: url('./img3.jpg');
 	}
 
 	.img4{
-		background-image: url('/img4.jpg');
+		background-image: url('./img4.jpg');
 	}
 
 	.img5{
-		background-image: url('/img5.jpg');
+		background-image: url('./img5.jpg');
 	}
 
 	.img6{
-		background-image: url('/img6.jpg');
+		background-image: url('./img6.jpg');
 	}
 
 	.img7{
-		background-image: url('/img7.jpg');
+		background-image: url('./img7.jpg');
 	}
 
 	.img8{
-		background-image: url('/img8.jpg');
+		background-image: url('./img8.jpg');
 	}
 
 	.img9{
-		background-image: url('/img9.jpg');
+		background-image: url('./img9.jpg');
 	}
 
 	nav li:hover div{
